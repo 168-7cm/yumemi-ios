5ae4305bec2ad65c9392bdae1341b8ce4755bba9
Kou Yamamoto <yama.121020@gmail.com>
2021-05-06 00:11:43 +0900

    NotificationCenterでアプリがforgroundに戻った際にAPIを呼ぶように実装

---
+++ yumemi-ios/Extension/Notification+Name.swift
@@ -0,0 +1,12 @@
+//
+//  Notification+Name.swift
+//  yumemi-ios
+//
+//  Created by kou yamamoto on 2021/05/05.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let foreground = Notification.Name("foreground")
+}

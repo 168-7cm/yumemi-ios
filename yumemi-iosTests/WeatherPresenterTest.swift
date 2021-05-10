1f56877f29290443c11bbf9f9603b8330e7e3951
Kou Yamamoto <yama.121020@gmail.com>
2021-05-10 17:06:29 +0900

    いらない箇所を削除

--- yumemi-iosTests/WeatherPresenterTest.swift
+++ yumemi-iosTests/WeatherPresenterTest.swift
@@ -31,10 +31,6 @@ class WeatherPresenterTests: XCTestCase {
             }
         }
     }
-
-    func testSunnyWeahterView() {
-        self.view.changeWeatherImageView(weather: "sunny")
-    }
 }
 
 // WeahterModelのモック
